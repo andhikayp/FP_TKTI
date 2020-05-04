1de01222bcd0e0acc436218e62d7b4626cb52e6b
Safhira Maharani <safhira.maharani14@gmail.com>
2020-05-04 14:42:42 +0700

    crud user

--- application/controllers/PetugasController.php
+++ application/controllers/PetugasController.php
@@ -62,9 +62,6 @@ class PetugasController extends MY_Protectedcontroller
 		$data['donasi'] = $this->query->all('user');
 		$this->slice->view('dashboard.profil.petugas.list', $data);
 	}
-	public function tambah(){
-		$this->slice->view('dashboard.profil.petugas.tambah');
-	}
 
 	public function verifikasi($id)
 	{
@@ -80,5 +77,55 @@ class PetugasController extends MY_Protectedcontroller
 			$this->session->set_flashdata('message', array('type' => 'success', 'message' => ['Proses Verifikasi Berhasil']));
 		}
 		return redirect(base_url('PetugasController/list_user'));	
+	}
+
+	public function tambahUser(){
+		if ($_SERVER['REQUEST_METHOD'] == "GET"){
+			$this->slice->view('dashboard.profil.petugas.tambah');
+		}
+		elseif($_SERVER['REQUEST_METHOD'] == "POST"){
+
+			$config['upload_path']          = './img/user';
+			$config['allowed_types']        = 'gif|jpg|png|jpeg';
+			$config['max_size']             = 30000;
+			$config['max_width']            = 1024;
+			$config['max_height']           = 768;
+			$config['overwrite']			= true;
+
+			$this->load->library('upload', $config);
+			$this->upload->do_upload('foto_ktp');
+			$this->upload->do_upload('foto_kk');
+			$this->upload->do_upload('foto_depan_rumah');
+
+			$insert_data = [
+				'role' => $this->input->post('role'),
+				'nama' => $this->input->post('nama'),
+				'email' => $this->input->post('email'),
+				'password' => $this->input->post('password'),
+                'no_telp' => $this->input->post('telp'),
+                'alamat' => $this->input->post('alamat'),
+				'foto_ktp' => $_FILES["foto_ktp"]["name"],
+				'foto_kk' => $_FILES["foto_kk"]["name"],
+				'foto_depan_rumah' => $_FILES["foto_depan_rumah"]["name"],
+            ];
+            try{
+                $this->db->insert('user', $insert_data);
+                $this->session->set_flashdata('message', array('type' => 'success', 'message' => ['Sukses Menambah Petugas Kasir']));
+				return redirect(base_url('PetugasController/list_user'));	
+            } catch(Exception $e){
+                $this->session->set_flashdata('message', array('type' => 'error', 'message' => [validation_errors()]));
+				$this->session->set_flashdata('post_data', $this->input->post(NULL, TRUE));
+				return redirect(base_url('PetugasController/tambahUser'));
+            }
+		}
+		else{
+			show_error("Method Not Allowed", 405);
+		}
+
+	}
+	public function deleteUser($id){
+		$this->users->deluser($id);
+		$this->session->set_flashdata('message', array('type' => 'success', 'message' => ['User Berhasil Dihapus']));
+		return redirect(base_url('PetugasController/list_user'));
 	}
 }
